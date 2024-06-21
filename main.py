2c97000c3382dbe39fd4b8ee05bc5aa574ce4e3c
Tomi Salami <salami.tomie@gmail.com>
2024-06-21 15:48:40 -0400

    Update code to include exception handling.

--- main.py
+++ main.py
@@ -5,8 +5,17 @@ import pandas
 nato_df = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter: row.code for (index, row) in nato_df.iterrows()}
 
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input('Enter a word: ').upper()
-nato_word_list = [nato_dict[letter] for letter in word]
 
-print(nato_word_list)
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+def create_nato_word():
+    word = input('Enter a word: ').upper()
+    try:
+        nato_word_list = [nato_dict[letter] for letter in word]
+    except KeyError:
+        print('Please only enter letters in the alphabet')
+        create_nato_word()
+    else:
+        print(nato_word_list)
+
+
+create_nato_word()
